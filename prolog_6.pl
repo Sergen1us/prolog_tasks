4dc4dbbc3034db4c36fbb6138a1f39084960bc49
Sergen1us <96681840+Sergen1us@users.noreply.github.com>
2021-12-27 12:36:57 +0700

    Update prolog_6.pl

--- prolog_6.pl
+++ prolog_6.pl
@@ -3,5 +3,5 @@ positions(_number, [X|Tail_1], _Positions):- X >= 0, _next_num is _number + 1, p
 positions(_number, [X|Tail_1], [_number|Tail_2]):- _next_num is _number + 1, positions(_next_num, Tail_1, Tail_2).
 
 %Пример: positions( 0, [ 1, 3, -2, 4, -6, 10], N).
-         Ответ: N = [ 2, 4].
+        %Ответ: N = [ 2, 4].
 
