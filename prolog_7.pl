7a848fdd45039e30d621eb07c4a24971b0195988
Sergen1us <96681840+Sergen1us@users.noreply.github.com>
2021-12-27 12:43:04 +0700

    Update prolog_7.pl

--- prolog_7.pl
+++ prolog_7.pl
@@ -1,3 +1,6 @@
 sublist( _, _, [], []).
 sublist(_position, _number, [X|Tail_1], _sublist):- _number < _position, _next_num is _number + 1, sublist(_position, _next_num, Tail_1, _sublist), !.
 sublist(_position, _number, [X|Tail_1], [X|Tail_2]):- _number >= _position, sublist(_position, _number, Tail_1, Tail_2).
+
+%Пример: sublist( 4, 1, [ 1, 2, 3, 4, 5, 6, 7, 8], N).
+%Ответ: N = [ 4, 5, 6, 7, 8].
