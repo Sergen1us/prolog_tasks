042f24489b0e62d49ea4663f96693af88ca478ea
Sergen1us <96681840+Sergen1us@users.noreply.github.com>
2021-12-30 02:54:42 +0700

    Update prolog_4(2).pl

--- prolog_4(2).pl
+++ prolog_4(2).pl
@@ -1,3 +1,6 @@
 change(_,_,[],[]).
 change(_old_value, _new_value, [X|Tail_1], [X|Tail_2]):- X =\= _old_value, change(_old_value, _new_value, Tail_1, Tail_2),!.
 change(_old_value, _new_value, [_old_value|Tail_1], [_new_value|Tail_2]):- change(_old_value, _new_value, Tail_1, Tail_2).
+
+%Пример: change(1,3, [1,2,3,4,1,2], N).
+        %N = [3,2,3,4,3,2].
