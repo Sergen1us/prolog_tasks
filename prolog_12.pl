23d5de93e111fe49adaf6f9ee1ee79d6844573ef
Sergen1us <96681840+Sergen1us@users.noreply.github.com>
2021-12-30 02:44:03 +0700

    Update prolog_12.pl

--- prolog_12.pl
+++ prolog_12.pl
@@ -2,3 +2,6 @@ scal(_first_vec,_second_vec, _ans):-
     scal(_first_vec, _second_vec, 0, _ans).
     scal([],[],S1, S1):-!.
     scal([X1|Tail_1], [X2|Tail_2], S, _ans):- S1 is S + X1*X2, scal(Tail_1,Tail_2, S1, _ans).
+    
+%Пример: scal([1,2],[3,4], A).
+         %A = 11.
