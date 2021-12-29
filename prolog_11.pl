d0dbd977864125c033f10f4f98fb99571846360d
Sergen1us <96681840+Sergen1us@users.noreply.github.com>
2021-12-30 02:42:15 +0700

    Update prolog_11.pl

--- prolog_11.pl
+++ prolog_11.pl
@@ -2,6 +2,6 @@ left(List, N, Slided):- append(Left, Right, List),length(Left, N),append(Right, 
 right(List, N, Slided):-append(Left, Right, List),length(Right, N),append(Right, Left, Slided), !.
 
 %Пример: left([1,2,3,4], 2, N).
-        N = [3,4,1,2].
-        right([1,2,3], 1, N).
-        N =([3,1,2]).
+        %N = [3,4,1,2].
+        %right([1,2,3], 1, N).
+        %N =([3,1,2]).
